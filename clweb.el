5e82d64f03d4a38d9f50981f98f9181cdd53fbc3
Alex Plotnick <plotnick@cs.brandeis.edu>
2009-02-15 21:37:56 +0000

    Added `clweb.el', an Emacs minor mode for working with CLWEB, with support function LOAD-SECTIONS-FROM-TEMP-FILE.  Also fixed return value for LOAD-WEB- FROM-STREAM, and changed *NAMED-SECTIONS* and *SECTION-NUMBER* definitions to use DEFVAR instead of DEFPARAMETER so that we don't lose them when loading "clweb.lw".
    git-svn-id: file:///home/alex/svn/src/clweb/trunk@800 1fe77fbb-760c-0410-9197-9ee16f777b24

---
+++ clweb.el
@@ -0,0 +1,46 @@
+;;;; A simple minor-mode for editing CLWEB programs.
+
+(defvar *start-section-regexp* "@ \\|@\\*\\|@\n")
+
+(defun move-to-section (arg)
+  (condition-case nil
+      (progn
+        (while (> arg 0)
+          (re-search-forward *start-section-regexp*)
+          (setq arg (1- arg)))
+        (while (< arg 0)
+          (re-search-backward *start-section-regexp*)
+          (setq arg (1+ arg))))
+    (search-failed (message "Can't find section boundary"))))
+
+(defun forward-section (arg)
+  "Advance past next WEB section beginning; with ARG, repeat ARG times."
+  (interactive "p")
+  (move-to-section (if (looking-at *start-section-regexp*) (1+ arg) arg)))
+
+(defun backward-section (arg)
+  "Advance to previous WEB section beginning; with ARG, repeat ARG times."
+  (interactive "p")
+  (move-to-section (- arg)))
+
+(defun eval-section ()
+  (interactive)
+  (let ((tmp (make-temp-file "clweb"))
+        (start (save-excursion
+                 (unless (looking-at *start-section-regexp*)
+                   (move-to-section -1))
+                 (point)))
+        (end (save-excursion
+               (move-to-section 1)
+               (- (point) 2))))
+    (write-region start end tmp t 'nomsg)
+    (comint-simple-send (inferior-lisp-proc)
+                        (format "(load-sections-from-temp-file %S)" tmp))))
+
+(define-minor-mode clweb-mode
+  "A minor mode for editing CLWEB programs."
+  :lighter " CLWEB"
+  :keymap '(("\C-\M-n" . forward-section)
+            ("\C-\M-p" . backward-section)
+            ("\C-c\C-s" . eval-section)))
+
