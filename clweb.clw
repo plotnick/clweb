plotnick/clweb

commit 50db203e083d3ee847c22bb222c97aee83b768d0

Fixed weaving of multi-line read-time conditionals.
We now emit read-time conditional forms one line at a time, like
we do with literal strings. To ensure consistency, we've also
removed the (trivial) optimization of eliding the alignment tabs
for simple forms. (Weaving a top-level multi-line string was also
broken due to this optimization, but we never noticed.)

clweb.clw
@@ -2230,7 +2230,7 @@ characters that the reader scans, and use that to reconstruct the form.
 
 (set-tangle-dispatch 'read-time-conditional
   (lambda (stream obj)
-    (format stream "#~:[-~;+~]~S ~A"
+    (format stream "#~:[-~;+~]~S~A"
             (read-time-conditional-plusp obj)
             (read-time-conditional-test obj)
             (read-time-conditional-form obj))))
@@ -2258,9 +2258,6 @@ characters that the reader scans, and use that to reconstruct the form.
                      (*read-suppress* nil))
                  (read stream t nil t)))
          (*read-suppress* (if plusp (not (featurep test)) (featurep test))))
-    (case (peek-char nil stream t nil t)
-      (#\Newline nil) ; preserve newline as part of the form
-      (t (peek-char t stream t nil t)))
     (read-with-echo (stream value form)
       (apply #'make-instance 'read-time-conditional-marker
              :plusp plusp :test test :form form
@@ -2280,7 +2277,7 @@ characters that the reader scans, and use that to reconstruct the form.
     (values (read-time-conditional-plusp conditional)
             (read-time-conditional-test conditional)
             (read-time-conditional-form conditional)))
-  nil :foo "1")
+  nil :foo " 1")
 
 (deftest (read-time-conditional 2)
   (let ((*features* '(:a))
@@ -3583,9 +3580,9 @@ or \.{\\N} depending on whether the section is unstarred or starred. Then
 comes the commentary, which is separated from the code part by a bit of
 vertical space using the \.{\\Y} macro if both are present. The code part
 always starts with \.{\\B}, followed by the name if it's a named section.
-Then comes the code, which we output one form at a time, using tabs for
-every line unless it's atomic. Last come the cross-references and a final
-\.{\\fi} that matches the \.{\\ifon} in \.{\\M} and \.{\\N}.
+Then comes the code, which we output one form at a time, using alignment
+tabs (\.{\\+}). Last come the cross-references and a final \.{\\fi} that
+matches the \.{\\ifon} in \.{\\M} and \.{\\N}.
 
 @l
 (defun print-section (stream section)
@@ -3606,9 +3603,9 @@ every line unless it's atomic. Last come the cross-references and a final
               (= (section-number section) (section-number named-section))))
     (when code
       (dolist (form code)
-        (if (or (list-marker-p form) (listp form))
-            (format stream "~@<\\+~@;~W~;\\cr~:>" form)
-            (format stream "~W~:[\\6~;~]" form (newlinep form))))
+        (if (newlinep form)
+            (terpri stream)
+            (format stream "~@<\\+~@;~W~;\\cr~:>" form)))
       (format stream "~&\\egroup~%")) ; matches \.{\\bgroup} in \.{\\B}
     (when (and (not named-section)
                (typep section 'test-section)
@@ -3901,8 +3898,8 @@ markers and the logical block structure.
 
 As an optimization, if we build a block that doesn't directly contain any
 newlines---a trivial logical block---we simply return a list of sub-forms
-instead of a logical block structure. This allows the printer to elide the
-alignment tabs, and makes the resulting \TeX\ much simpler.
+instead of a logical block structure. This can make the resulting \TeX\
+somewhat simpler.
 
 @<Build a logical block...@>=
 (do* ((block '())
@@ -4084,14 +4081,43 @@ which see.
   (lambda (stream obj)
     (format stream "\\#S~W" (structure-marker-form obj))))
 
-@ @l
+@ Read-time conditionals, like literal strings, must be printed one line at
+a time so as to respect the alignment tabs. Since the \.{\\RC} macro switches
+to a monospaced font and activates \.{\\obeyspaces}, dealing with indentation
+is straightforward.
+
+@l
 (set-weave-dispatch 'read-time-conditional-marker
   (lambda (stream obj)
-    (let ((*print-escape-list* (acons " " " " *print-escape-list*)))
-      (format stream "\\#~:[--~;+~]~S{\\RC ~/clweb::print-escaped/}"
-              (read-time-conditional-plusp obj)
-              (read-time-conditional-test obj)
-              (read-time-conditional-form obj)))))
+    (let* ((form (read-time-conditional-form obj))
+           (lines @<Split |form| into lines, eliding any initial indentation@>)
+           (*print-escape-list* `((" " . " ") ,@*print-escape-list*)))
+      (flet ((print-rc-prefix ()
+               (format stream "\\#$~:[-~;+~]$~S"
+                       (read-time-conditional-plusp obj)
+                       (read-time-conditional-test obj)))
+             (print-rc-line (line)
+               (format stream "\\RC{~/clweb::print-escaped/}" line)))
+        (cond ((= (length lines) 1)
+               (print-rc-prefix)
+               (print-rc-line (first lines)))
+              (t (write-string "\\!" stream)
+                 (pprint-logical-block (stream lines :per-line-prefix "&")
+                   (print-rc-prefix)
+                   (pprint-exit-if-list-exhausted)
+                   (loop for line = (pprint-pop)
+                         when (plusp (length line)) do (print-rc-line line)
+                         do (pprint-exit-if-list-exhausted)
+                            (format stream "\\cr~:@_")))))))))
+
+@ @<Split |form| into lines...@>=
+(loop with indent = (if (char= (elt form 0) #\Newline)
+                        (1- (position-if-not #'whitespacep form :start 1))
+                        0)
+      for last = 0 then (+ indent newline 1)
+      for newline = (position #\Newline form :start last)
+      as line = (subseq form last newline)
+      collect line while newline)
 
 @*Code walking. Our last major task is to produce an index of every
 interesting symbol that occurs in a web (we'll define what makes a

clweb.lisp
@@ -23,7 +23,7 @@
            "SECTION-NAME-DEFINITION-ERROR"
            "UNUSED-NAMED-SECTION-WARNING")
   (:SHADOW #+(:OR :ALLEGRO :CCL) "MAKE-PATHNAME")
-  #+(:OR :SBCL :CCL :ALLEGRO) 
+  #+(:OR :SBCL :CCL :ALLEGRO)
   (:import-from #+sbcl "SB-CLTL2"
                 #+ccl "CCL"
                 #+allegro "SYS"
@@ -833,7 +833,7 @@
            (FORM :READER READ-TIME-CONDITIONAL-FORM :INITARG :FORM)))
 (SET-TANGLE-DISPATCH 'READ-TIME-CONDITIONAL
                      (LAMBDA (STREAM OBJ)
-                       (FORMAT STREAM "#~:[-~;+~]~S ~A"
+                       (FORMAT STREAM "#~:[-~;+~]~S~A"
                                (READ-TIME-CONDITIONAL-PLUSP OBJ)
                                (READ-TIME-CONDITIONAL-TEST OBJ)
                                (READ-TIME-CONDITIONAL-FORM OBJ))))
@@ -860,9 +860,6 @@
           (IF PLUSP
               (NOT (FEATUREP TEST))
               (FEATUREP TEST))))
-    (CASE (PEEK-CHAR NIL STREAM T NIL T)
-      (#\Newline NIL)
-      (T (PEEK-CHAR T STREAM T NIL T)))
     (READ-WITH-ECHO (STREAM VALUE FORM)
       (APPLY #'MAKE-INSTANCE 'READ-TIME-CONDITIONAL-MARKER :PLUSP PLUSP :TEST
              TEST :FORM FORM (AND (NOT *READ-SUPPRESS*) (LIST :VALUE VALUE))))))
@@ -1164,7 +1161,7 @@ otherwise, they will replace them."
         (WITH-OPEN-FILE (STREAM FILE :DIRECTION :INPUT)
           (LOAD-WEB-FROM-STREAM STREAM T :APPEND APPEND))
       (DELETE-FILE FILE))))
-#+(:OR :ALLEGRO :CCL) 
+#+(:OR :ALLEGRO :CCL)
 (defun make-pathname (&key host device directory name type version
                       (defaults
                        (cl:make-pathname :host (pathname-host
@@ -1441,9 +1438,9 @@ otherwise, they will replace them."
               (= (SECTION-NUMBER SECTION) (SECTION-NUMBER NAMED-SECTION))))
     (WHEN CODE
       (DOLIST (FORM CODE)
-        (IF (OR (LIST-MARKER-P FORM) (LISTP FORM))
-            (FORMAT STREAM "~@<\\+~@;~W~;\\cr~:>" FORM)
-            (FORMAT STREAM "~W~:[\\6~;~]" FORM (NEWLINEP FORM))))
+        (IF (NEWLINEP FORM)
+            (TERPRI STREAM)
+            (FORMAT STREAM "~@<\\+~@;~W~;\\cr~:>" FORM)))
       (FORMAT STREAM "~&\\egroup~%"))
     (WHEN
         (AND (NOT NAMED-SECTION) (TYPEP SECTION 'TEST-SECTION)
@@ -1697,20 +1694,54 @@ otherwise, they will replace them."
                       (FORMAT STREAM "\\#S~W" (STRUCTURE-MARKER-FORM OBJ))))
 (SET-WEAVE-DISPATCH 'READ-TIME-CONDITIONAL-MARKER
                     (LAMBDA (STREAM OBJ)
-                      (LET ((*PRINT-ESCAPE-LIST*
-                             (ACONS " " " " *PRINT-ESCAPE-LIST*)))
-                        (FORMAT STREAM
-                                "\\#~:[--~;+~]~S{\\RC ~/clweb::print-escaped/}"
-                                (READ-TIME-CONDITIONAL-PLUSP OBJ)
-                                (READ-TIME-CONDITIONAL-TEST OBJ)
-                                (READ-TIME-CONDITIONAL-FORM OBJ)))))
+                      (LET* ((FORM (READ-TIME-CONDITIONAL-FORM OBJ))
+                             (LINES
+                              (LOOP WITH INDENT = (IF (CHAR= (ELT FORM 0)
+                                                             #\Newline)
+                                                      (1-
+                                                       (POSITION-IF-NOT
+                                                        #'WHITESPACEP FORM
+                                                        :START 1))
+                                                      0)
+                                    FOR LAST = 0 THEN (+ INDENT NEWLINE 1)
+                                    FOR NEWLINE = (POSITION #\Newline FORM
+                                                            :START
+                                                            LAST) AS LINE = (SUBSEQ
+                                                                             FORM
+                                                                             LAST
+                                                                             NEWLINE)
+                                    COLLECT LINE
+                                    WHILE NEWLINE))
+                             (*PRINT-ESCAPE-LIST*
+                              `((" " . " ") ,@*PRINT-ESCAPE-LIST*)))
+                        (FLET ((PRINT-RC-PREFIX ()
+                                 (FORMAT STREAM "\\#$~:[-~;+~]$~S"
+                                         (READ-TIME-CONDITIONAL-PLUSP OBJ)
+                                         (READ-TIME-CONDITIONAL-TEST OBJ)))
+                               (PRINT-RC-LINE (LINE)
+                                 (FORMAT STREAM "\\RC{~/clweb::print-escaped/}"
+                                         LINE)))
+                          (COND
+                           ((= (LENGTH LINES) 1) (PRINT-RC-PREFIX)
+                            (PRINT-RC-LINE (FIRST LINES)))
+                           (T (WRITE-STRING "\\!" STREAM)
+                            (PPRINT-LOGICAL-BLOCK
+                                (STREAM LINES :PER-LINE-PREFIX "&")
+                              (PRINT-RC-PREFIX)
+                              (PPRINT-EXIT-IF-LIST-EXHAUSTED)
+                              (LOOP FOR LINE = (PPRINT-POP)
+                                    WHEN (PLUSP (LENGTH LINE))
+                                    DO (PRINT-RC-LINE LINE)
+                                    DO (PPRINT-EXIT-IF-LIST-EXHAUSTED) (FORMAT
+                                                                        STREAM
+                                                                        "\\cr~:@_")))))))))
 (DEFUN ENSURE-PORTABLE-WALKING-ENVIRONMENT (ENV)
   #+:ALLEGRO (sys:ensure-portable-walking-environment env)
   #-:ALLEGRO env)
 (DEFUN ENCLOSE
        (LAMBDA-EXPRESSION &OPTIONAL ENV (WALKER (MAKE-INSTANCE 'WALKER)))
   (COERCE (WALK-LAMBDA-EXPRESSION WALKER LAMBDA-EXPRESSION NIL ENV) 'FUNCTION))
-#+:ALLEGRO 
+#+:ALLEGRO
 (defun parse-macro (name lambda-list body &optional env)
   (declare (ignorable name lambda-list body env))
   (excl::defmacro-expander `(,name ,lambda-list ,@body) env))
@@ -1720,9 +1751,9 @@ otherwise, they will replace them."
          (APPLY ,ORIG ARGS)
        (DECLARE (IGNORE LOCATIVE))
        (VALUES TYPE LOCAL DECLARATIONS))))
-#+:ALLEGRO 
+#+:ALLEGRO
 (reorder-env-information variable-information #'sys:variable-information)
-#+:ALLEGRO 
+#+:ALLEGRO
 (reorder-env-information function-information #'sys:function-information)
 (DEFCLASS WALK-CONTEXT NIL NIL)
 (DEFUN MAKE-CONTEXT (CONTEXT &REST ARGS) (APPLY #'MAKE-INSTANCE CONTEXT ARGS))

clweb-tests.lisp
@@ -371,7 +371,7 @@
            (VALUES (READ-TIME-CONDITIONAL-PLUSP CONDITIONAL)
                    (READ-TIME-CONDITIONAL-TEST CONDITIONAL)
                    (READ-TIME-CONDITIONAL-FORM CONDITIONAL)))
-         NIL :FOO "1")
+         NIL :FOO " 1")
 (DEFTEST (READ-TIME-CONDITIONAL 2)
          (LET ((*FEATURES* '(:A)) (*EVALUATING* T))
            (VALUES (MARKER-VALUE (READ-FORM-FROM-STRING "#+a 1"))

clwebmac.tex
@@ -49,7 +49,7 @@
 \def\CO#1{\leavevmode,#1\kern.025em} % comma(-at, -dot)
 \def\L{\leavevmode$\lambda$}
 \def\K#1{\hbox{\it#1\/\kern.05em}} % lambda-list keyword
-\def\RC{\obeyspaces\obeylines\tt} % read-time conditional
+\def\RC#1{\hbox{\obeyspaces\tt #1}} % read-time conditional
 \def\T{\exnote{This test exercises code in or around {\it\progname\/} section}}
 \def\Ts{\exnote{These tests exercise code in or around {\it\progname\/} section}}
 \def\X#1:#2\X{\ifmmode\gdef\XX{\null$\null}\else\gdef\XX{}\fi %$% section name

test.clw
@@ -115,6 +115,20 @@ newlines and such are ignored:
     (when (endp (cdr fast)) (return (+ n 1)))
     (when (and (eq fast slow) (> n 0)) (return nil))))
 
+@ @l
+"Here's a top-level string
+split over two lines."
+
+@ Read-time conditionals are also tricky, especially when they span
+multiple lines.
+
+@l
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  #+(or foo bar baz)
+  (frob this
+        that
+        and-another))
+
 @*Index tests.
 
 @ @l

commit 64d933ec907ae47c3c5ad0e491ea1daaa7471540

Fixed weaving of newline markers in code sections.
Commit 50db203e083d3ee847c22bb222c97aee83b768d0 broke this; we
failed to remember that par-marker is a subclass of newline-marker,
but is printed differently.

clweb.clw
@@ -3639,9 +3639,9 @@ matches the \.{\\ifon} in \.{\\M} and \.{\\N}.
               (= (section-number section) (section-number named-section))))
     (when code
       (dolist (form code)
-        (if (newlinep form)
-            (terpri stream)
-            (format stream "~@<\\+~@;~W~;\\cr~:>" form)))
+        (typecase form
+          (newline-marker (format stream "~W" form))
+          (t (format stream "~@<\\+~@;~W~;\\cr~:>" form))))
       (format stream "~&\\egroup~%")) ; matches \.{\\bgroup} in \.{\\B}
     (when (and (not named-section)
                (typep section 'test-section)

clweb.lisp
@@ -1459,9 +1459,9 @@ otherwise, they will replace them."
               (= (SECTION-NUMBER SECTION) (SECTION-NUMBER NAMED-SECTION))))
     (WHEN CODE
       (DOLIST (FORM CODE)
-        (IF (NEWLINEP FORM)
-            (TERPRI STREAM)
-            (FORMAT STREAM "~@<\\+~@;~W~;\\cr~:>" FORM)))
+        (TYPECASE FORM
+          (NEWLINE-MARKER (FORMAT STREAM "~W" FORM))
+          (T (FORMAT STREAM "~@<\\+~@;~W~;\\cr~:>" FORM))))
       (FORMAT STREAM "~&\\egroup~%"))
     (WHEN
         (AND (NOT NAMED-SECTION) (TYPEP SECTION 'TEST-SECTION)